a9ab6a0eee7b970f2a94c5014e8a6a788e49f233
Koenig-Heinrich-der-4te <84435773+Koenig-Heinrich-der-4te@users.noreply.github.com>
2023-12-17 20:26:34 +0100

    day 17 final commit?

--- 17.py
+++ 17.py
@@ -10,7 +10,7 @@ height = len(city_heat_loss_map)
 width = len(city_heat_loss_map[0])
 
 
-def explore(turn_moves, no_turn_moves=0):
+def explore(moves, no_turn_moves=0):
     target = (width - 1, height - 1)
     queue = [(0, 0, 0, (1, 0))]
     explored = {(0, 0, (1, 0)): 0}
@@ -22,18 +22,14 @@ def explore(turn_moves, no_turn_moves=0):
         # this path is already too expensive
         if cost >= best or explored[(x, y, d)] < cost:
             continue
-        # movement before turing is allowed
-        for _ in range(no_turn_moves):
+        for i in range(moves):
             x, y = x + d[0], y + d[1]
             if not (0 <= x < width and 0 <= y < height):
                 break
             cost += city_heat_loss_map[y][x]
-        # movement after turning is allowed
-        for _ in range(turn_moves):
-            x, y = x + d[0], y + d[1]
-            if not (0 <= x < width and 0 <= y < height):
-                break
-            cost += city_heat_loss_map[y][x]
+            # check wether we can turn yet
+            if i < no_turn_moves:
+                continue
             # explore left and right
             for dir in ((-d[1], -d[0]), (d[1], d[0])):
                 if (x, y, dir) not in explored or explored[(x, y, dir)] > cost:
@@ -47,7 +43,7 @@ print(
     f"(Part 1) The smallest heat loss for large crucibles is {large_loss} units"
 )  # 1008
 
-ultra_loss = explore(7, 3)
+ultra_loss = explore(10, 3)
 print(
     f"(Part 2) The smallest heat loss for ultra crucibles is {ultra_loss} units"
 )  # 1210
